3bd9a46d3368797491bbe88e16a2a473e393e7f3
Cea Stapleton <chelsea.j.stapleton@gmail.com>
2014-11-24 13:22:53 -0600

    Enter to continue

--- app/js/fhtagnResponseDirective.js
+++ app/js/fhtagnResponseDirective.js
@@ -6,29 +6,15 @@ app.directive('fhtagnResponse', function() {
       renderNext : "&"
     },
     templateUrl : './templates/fhtagn-response.html',
-    controller : ['$scope', function($scope) {
+    controller : ['$scope', '$rootScope', function($scope, $rootScope) {
       $scope.select = function() {
         $scope.renderNext({});
-      }
-    }],
-    link: function($rootScope) {
-        $rootScope.keyPressed = 'no press :(';
-        // For listening to a keypress event with a specific code
-        $rootScope.$on('keypress:13', function(onEvent, keypressEvent) {
-          $rootScope.keyPressed = 'Enter';
-          console.log($rootScope.keyPressed);
-        });
-        // For listening to all keypress events
-        $rootScope.$on('keypress', function(onEvent, keypressEvent) {
-          if (keypressEvent.which === 120) {
-            $rootScope.keyPressed = 'x';
-            console.log($rootScope.keyPressed);
-          }
-          else {
-            $rootScope.keyPressed = 'Keycode: ' + keypressEvent.which;
-            console.log($rootScope.keyPressed);
-          }
-        });
-      }
+      };
+      $rootScope.$on('keypress:13', function(onEvent, keypressEvent) {
+        $rootScope.keyPressed = 'Enter';
+        console.log("hereeee");
+        $scope.$apply($scope.renderNext());
+      });
+    }]
   };
 });
--- app/js/fhtagnStatementDirective.js
+++ app/js/fhtagnStatementDirective.js
@@ -6,10 +6,15 @@ app.directive('fhtagnStatement', function() {
       renderNext : "&"
     },
     templateUrl : './templates/fhtagn-statement.html',
-    controller : ['$scope', function($scope) {
+    controller : ['$scope', '$rootScope', function($scope, $rootScope) {
       $scope.select = function() {
         $scope.renderNext({});
-      }
-    }]
+      };
+      $rootScope.$on('keypress:13', function(onEvent, keypressEvent) {
+        $rootScope.keyPressed = 'Enter';
+        console.log("hereeee");
+        $scope.$apply($scope.renderNext());
+      });
+  }]
   };
 });
